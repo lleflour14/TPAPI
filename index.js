8b8080d164ca04313da3ae68bb9195118f8ba461
lleflour14 <louise.leflour@sfr.fr>
2025-10-01 17:49:06 +0200

    test

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const app = express();
 
 // Route GET basique
 app.get("/", (req, res) => {
-  res.send("Hello Azure 🚀");
+    res.send("test api");
 });
 
 // Autre route de test
 app.get("/ping", (req, res) => {
-  res.json({ message: "pong" });
+    res.json({ message: "pong" });
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ Server running on port ${PORT}`);
+    console.log(`✅ Server running on port ${PORT}`);
 });
